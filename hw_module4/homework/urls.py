789f41779a9f5b01c23fa7dba08e39096cd95e8f
Павел Гиль <gill1488@mail.ru>
2022-06-23 17:11:23 +0000

    diploma 23.06.2022

--- hw_module4/homework/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('advertisements', views.AdvertisementListView.as_view()),
-    path('advertisements/<int:pk>', views.AdvertisementDetailView.as_view()),
-]
