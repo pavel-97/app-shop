484a08c568f46940b7520559bc70d61099b029ca
pavel-97 <43730985+pavel-97@users.noreply.github.com>
2022-09-02 21:49:39 +0300

    Delete module14/project directory

--- module14/project/app_goods/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('items/', views.ItemListView.as_view(), name='item_list'),
-    path('items/<int:pk>/', views.ItemDetailView.as_view(), name='item_list'),
-    path('categories/', views.CategoryListView.as_view(), name='categories'),
-]
--- module14/project/app_user/api.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import viewsets
-from django.contrib.auth.models import User
-
-from . import serializers
-
-
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = serializers.UserSerializer
